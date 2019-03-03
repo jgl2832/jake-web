522ea261632693f721e09ac66fb144bb23876e91
Jake Levine <jgl2832@gmail.com>
2019-03-03 20:48:10 +0100

    restyle

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFacebookF, faTwitter, faLinkedin, faGithub, faInstagram } from '@fortawesome/free-brands-svg-icons'
+import { faEnvelope } from '@fortawesome/free-regular-svg-icons'
 import './App.css';
 
 class App extends Component {
@@ -7,18 +9,46 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hello there!
+          <h1 className="Name">
+            Jake G Levine
+          </h1>
+          <p className="Bio">
+            Software Engineer from NYC | Living in Berlin
+            <br/>
+            Staff Software Engineer @ Meetup
           </p>
-          <a
-            className="App-link"
-            href="http://jgl.nyc/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            jgl.nyc
-          </a>
+          <ul className="SocialLinks">
+            <li>
+              <a href="mailto:jgl@jgl.nyc" target="_blank">
+                <FontAwesomeIcon className="Icon" icon={faEnvelope} />
+              </a>
+            </li>
+            <li>
+              <a href="https://www.facebook.com/jgl2832" target="_blank">
+                <FontAwesomeIcon className="Icon" icon={faFacebookF} />
+              </a>
+            </li>
+            <li>
+              <a href="https://twitter.com/theDJ" target="_blank">
+                <FontAwesomeIcon className="Icon" icon={faTwitter} />
+              </a>
+            </li>
+            <li>
+              <a href="https://www.linkedin.com/profile/view?id=61732651" target="_blank">
+                <FontAwesomeIcon className="Icon" icon={faLinkedin} />
+              </a>
+            </li>
+            <li>
+              <a href="https://github.com/jgl2832" target="_blank">
+                <FontAwesomeIcon className="Icon" icon={faGithub} />
+              </a>
+            </li>
+            <li>
+              <a href="https://instagram.com/jgl2832/" target="_blank">
+                <FontAwesomeIcon className="Icon" icon={faInstagram} />
+              </a>
+            </li>
+          </ul>
         </header>
       </div>
     );
