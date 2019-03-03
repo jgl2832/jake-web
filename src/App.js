971db90e26be5d1c22f5e757a9d3ecd3dfdbf797
Jake Levine <jgl2832@gmail.com>
2019-03-03 18:54:47 +0100

    content change

--- src/App.js
+++ src/App.js
@@ -9,15 +9,15 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Hello there!
           </p>
           <a
             className="App-link"
-            href="https://reactjs.org"
+            href="http://jgl.nyc/"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            jgl.nyc
           </a>
         </header>
       </div>
