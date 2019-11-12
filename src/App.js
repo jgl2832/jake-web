8916d3db177b9a887ab7bb0d8f5050f04d707ad4
Jake Levine <jgl2832@gmail.com>
2019-11-12 22:25:37 +0100

    talks page

--- src/App.js
+++ src/App.js
@@ -17,20 +17,20 @@ class App extends Component {
       <Router>
         <div className="App">
           <header className="App-header">
+            <p className="Talks">
+              <Link to="/">Home</Link> | <Link to="/talks">Talks</Link>
+            </p>
             <h1 className="Name">
               Jake G Levine
             </h1>
             <Switch>
               <Route path="/talks">
-                <div>sup</div>
+                <Talks />
               </Route>
               <Route path="/">
                 <Home />
               </Route>
             </Switch>
-            <p className="Talks">
-              <Link to="/">Home</Link> | <Link to="/talks">Talks</Link>
-            </p>
           </header>
         </div>
       </Router>
@@ -38,9 +38,36 @@ class App extends Component {
   }
 }
 
+function Talks() {
+  return (
+    <span class="Talks">
+      <label for="codemotion">
+        Lazy Engineering (<a href="https://events.codemotion.com/conferences/berlin/2019/speaker/6025/" target="_blank" rel="noopener noreferrer">Codemotion Berlin 2019</a>)
+      </label>
+      <div id="codemotion">Coming soon!</div>
+      <br />
+      <label for="bangbangcon">
+        Sorting is as easy as 1,2,3 - but not as easy as a,b,c! (<a href="http://bangbangcon.com/2016/speakers.html#jake-levine" target="_blank" rel="noopener noreferrer">!!con 2016</a>)
+      </label>
+      <br />
+      <div class="videoWrapper">
+        <iframe title="bangbangcon" id="bangbangcon" width="560" height="315" src="https://www.youtube.com/embed/kmLHuKs0M10" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      </div>
+      <br />
+      <label for="devpost">
+        All about phone interviews (Devpost)
+      </label>
+      <br />
+      <div class="videoWrapper">
+        <iframe title="devpost" id="devpost" width="560" height="315" src="https://www.youtube.com/embed/JnJpmuhupuQ" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      </div>
+    </span>
+  );
+}
+
 function Home() {
   return (
-    <span className="Home">
+    <React.Fragment>
       <p className="Bio">
         Software Engineer in Berlin | From NYC
       </p>
@@ -76,7 +103,7 @@ function Home() {
           </a>
         </li>
       </ul>
-    </span>
+    </React.Fragment>
   );
 }
 
