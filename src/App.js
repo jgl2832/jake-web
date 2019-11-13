e823226faab2124a19f0f0abbe16cceba035aa7a
Jake Levine <jgl2832@gmail.com>
2019-11-13 22:32:20 +0100

    fix up

--- src/App.js
+++ src/App.js
@@ -13,19 +13,18 @@ function App() {
   return (
     <Router>
       <div className="App">
+        <p className="Links">
+          <Link to="/">Home</Link> | <Link to="/talks">Talks</Link>
+        </p>
         <header className="App-header">
-          <p className="Links">
-            <Link to="/">Home</Link> | <Link to="/talks">Talks</Link>
-          </p>
           <h1 className="Name">
             Jake G Levine
           </h1>
+          <Home />
           <Switch>
             <Route path="/talks">
+              <hr className="line" />
               <Talks />
-            </Route>
-            <Route path="/">
-              <Home />
             </Route>
           </Switch>
         </header>
