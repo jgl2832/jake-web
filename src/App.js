e043590d64e322de1f5b104f23f5cc9e777fda40
Jake Levine <jgl2832@gmail.com>
2019-11-14 10:55:54 +0100

    more cleaning

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link,
 } from "react-router-dom";
 import './App.css';
 import Home from './Home';
@@ -15,7 +14,7 @@ function App() {
     <Router>
       <div className="App">
         <Nav />
-        <header className="Content">
+        <div className="Content">
           <h1 className="Name">
             Jake G Levine
           </h1>
@@ -26,7 +25,7 @@ function App() {
               <Talks />
             </Route>
           </Switch>
-        </header>
+        </div>
       </div>
     </Router>
   );
--- src/Nav.jsx
+++ src/Nav.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 export default function Nav() {
   return (
-    <p className="Links">
+    <p className="Nav">
       <Link to="/">Home</Link> | <Link to="/talks">Talks</Link>
     </p>
   );
