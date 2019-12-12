9ec880456431f004132299f8cdfdc5cb867e528c
Jake Levine <jgl2832@gmail.com>
2019-12-12 20:44:27 +0100

    add link to codemotion video and blog post

--- src/Talks.js
+++ src/Talks.js
@@ -18,6 +18,10 @@ const lazyEngLink = <ExternalLink link="https://events.codemotion.com/conference
   Codemotion Berlin 2019
 </ExternalLink>
 
+const lazyEngBlogLink = <ExternalLink link="https://medium.com/@codemotion/codemotion-berlin-shows-the-future-of-programming-work-and-society-9b723a8a4baa#0f1e">
+  Recap
+</ExternalLink>
+
 const bangBangLink = <ExternalLink link="http://bangbangcon.com/2016/speakers.html#jake-levine">
   !!con 2016
 </ExternalLink>
@@ -26,9 +30,10 @@ export default function Talks() {
   return (
     <span className="Talks">
       <label for="codemotion">
-        Lazy Engineering ({lazyEngLink}))
+        Lazy Engineering ({lazyEngLink} - {lazyEngBlogLink})
       </label>
-      <div id="codemotion">Coming soon!</div>
+      <br />
+      <Video id="codemotion" url="https://www.youtube.com/embed/UXSSJENiZiw" />
       <br />
       <label for="bangbangcon">
         Sorting is as easy as 1,2,3 - but not as easy as a,b,c! ({bangBangLink})
